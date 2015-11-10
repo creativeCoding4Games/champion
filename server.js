fdc543e9b408f4731dd6a019677f35511e83750c
Jason Francis <jason@halfspiral.com>
2015-11-10 14:23:44 -0800

    Fix for express static serve

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var path= require('path');
 var app = express();
+var port = process.env.HTTP_PORT || 3050;
 
-app.use(express.static('champion.css'));
-app.use(express.static('index.html'));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen(3050);
+app.listen(port);
+console.log(`Listening on port ${port}`);
